305614df312c9f1adf6705f58aad035607ab231c
PoppyDory <lanza.rosita@gmail.com>
2023-02-13 15:15:57 +0100

    esercizi

---
+++ numeriPari0_20.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <= 20; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << "\n";
+        }
+    }
+}
---
+++ somma0_100.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int numero=0;
+
+    for (int i=0; i<=100; i++)
+    {
+        numero+= i;
+    }
+    cout<< numero;
+}
+
