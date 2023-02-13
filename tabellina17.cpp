1ed4ca51938f4f8e788d9e50ff3df54c527168f4
PoppyDory <lanza.rosita@gmail.com>
2023-02-13 13:55:49 +0100

    esercizi corretti

---
+++ tabellina17.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero = 7;
+    
+
+    for (int i = 0; i <= 10; i++)
+    {
+        cout<< numero*i << "\n";
+    
+    }
+}
