cdd34f9e599f248ea572e4f591c6a8daebde019d
PoppyDory <lanza.rosita@gmail.com>
2023-03-03 19:06:19 +0100

    nuovo

---
+++ esercizio.cpp
@@ -0,0 +1,19 @@
+//scivere un programma che riceve in input numeri interi non nulli
+//0 per terminare, stampa se ogni numero ha anche il sto valore opposto
+//
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main ()
+{
+    int array[100];
+    int numero=0;
+
+    for(int i=0; (i<100 && array[i]!=0); i++)
+    {
+        cin >> array
+    }
+}
